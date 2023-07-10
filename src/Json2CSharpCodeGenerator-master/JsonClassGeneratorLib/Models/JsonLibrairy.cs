dd2af973084d3f8207f5d05b9a744eacc2a42f46
biegehydra <connorhallman29@gmail.com>
2023-07-10 13:42:00 -0400

    json2csharp+ start

--- src/Json2CSharpCodeGenerator-master/JsonClassGeneratorLib/Models/JsonLibrairy.cs
+++ src/Json2CSharpCodeGenerator-master/JsonClassGeneratorLib/Models/JsonLibrairy.cs
@@ -8,11 +8,11 @@ namespace Xamasoft.JsonClassGenerator.Models
 {
     public enum JsonLibrary
     {
-        /// <summary>Use the <see cref="Newtonsoft.Json.JsonPropertyAttribute"/> on generated C# class properties.</summary>
-        NewtonsoftJson,
-
         /// <summary>Use the <c>[JsonPropertyName]</c> attribute on generated C# class properties.</summary>
         SystemTextJson,
+
+        /// <summary>Use the <see cref="Newtonsoft.Json.JsonPropertyAttribute"/> on generated C# class properties.</summary>
+        NewtonsoftJson,
     }
 
 }
